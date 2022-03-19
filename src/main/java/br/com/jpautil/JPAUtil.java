f3bfa38b1a4902e09901c1b125cca58c5b86d0f6
maxmillionsmax <maximilianocaetano@gmail.com>
2022-03-19 15:48:35 -0300

    criando classe JPAUtil, controle transação banco de dados

---
+++ src/main/java/br/com/jpautil/JPAUtil.java
@@ -0,0 +1,20 @@
+package br.com.jpautil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+	private static EntityManagerFactory factory = null;
+	
+	static {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("projetojsf");
+		}
+	}
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+}
