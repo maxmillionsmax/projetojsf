fc2004f925bc8066b5aca9556c5eb9cb0012776a
maxmillionsmax <maximilianocaetano@gmail.com>
2022-03-19 01:50:14 -0300

    implementando JPA e o Hibernate com Maven

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,27 +1,70 @@
 package br.com.cursojsf;
 
-import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+@ViewScoped
 @ManagedBean(name = "pessoaBean")
 public class PessoaBean {
 
 	private String nome;
-	private String sobrenome;
+	
+	private String senha;
+	
+	private String texto;
+	
+	private HtmlCommandButton commandButton;
+	
+	private List<String> nomes = new ArrayList<String>();
+	
+	public String addNome() {
+		nomes.add(nome);
+		if (nomes.size() > 2) {
+			commandButton.setDisabled(true);
+			return "paginanavegada?faces-redirect=true";
+		}
+		
+		return "";
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+	
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
 
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+	public List<String> getNomes() {
+		return nomes;
+	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
 	public String getNome() {
 		return nome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
 	}
 
 }
