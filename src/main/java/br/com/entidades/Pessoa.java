a8e9ba7bbd0d7e249d2ce1c906b03a38aa29da73
maxmillionsmax <maximilianocaetano@gmail.com>
2022-03-23 14:46:01 -0300

    Utilizando SelectManyCheckbox

--- src/main/java/br/com/entidades/Pessoa.java
+++ src/main/java/br/com/entidades/Pessoa.java
@@ -42,11 +42,21 @@ public class Pessoa implements Serializable {
 	private String perfilUser;
 
 	private String nivelProgramador;
+	
+	private Integer[] linguagens;
 
 	public Pessoa() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void setLinguagens(Integer[] linguagens) {
+		this.linguagens = linguagens;
+	}
+	
+	public Integer[] getLinguagens() {
+		return linguagens;
+	}
+	
 	public void setNivelProgramador(String nivelProgramador) {
 		this.nivelProgramador = nivelProgramador;
 	}
